3d413e2d4a999a641be3d7ee936d1c84ada1d50d
Hanish-H <hamish.h@outlook.com>
2018-04-21 02:08:27 +0100

    added more songs

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -7,4 +7,12 @@ require_relative('../song.rb')
 
 class GuestTest < MiniTest::Test
 
+  def setup
+    @guest1 = Guest.new("Tom")
+    @guest2 = Guest.new("Dick")
+    @guest3 = Guest.new("Harry")
+    @guest4 = Guest.new("George")
+  end
+
+
 end
--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -7,10 +7,35 @@ require_relative('../song.rb')
 
 class SongTest < MiniTest::Test
   def setup
-    @song_name = Song.new('Bohemian Rhapsody')
+    @song1_name = Song.new('Bohemian Rhapsody')
+    @song2_name = Song.new('Flash')
+    @song3_name = Song.new('Barcelona')
+    @song4_name = Song.new('Somebody to Love')
+    @song5_name = Song.new('Killer Queen')
   end
 
-  def test_song_name
-    assert_equal('Bohemian Rhapsody', @song_name.song_name())
+  def test_song1_name
+    assert_equal('Bohemian Rhapsody',
+      @song1_name.song_name())
+  end
+
+  def test_song2_name
+    assert_equal('Flash',
+      @song2_name.song_name())
+  end
+
+  def test_song3_name
+    assert_equal('Barcelona',
+      @song3_name.song_name())
+  end
+
+  def test_song4_name
+    assert_equal('Somebody to Love',
+      @song4_name.song_name())
+  end
+
+  def test_song5_name
+    assert_equal('Killer Queen',
+      @song5_name.song_name())
   end
 end
