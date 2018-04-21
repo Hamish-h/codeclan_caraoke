f8a2189662c6d25bf3a479b448721d7a1c0e069d
Hanish-H <hamish.h@outlook.com>
2018-04-21 22:47:50 +0100

    reinitialized room setup and re-structured code

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -8,19 +8,23 @@ require_relative('../song.rb')
 class RoomTest < MiniTest::Test
 
   def setup
-    @guest1_name = Guest.new('Tom')
-    @guest2_name = Guest.new('Dick')
-    @guest3_name = Guest.new('Harry')
-    @guest4_name = Guest.new('George')
+    @guest_1 = Guest.new('Tom')
+    @guest_2 = Guest.new('Dick')
+    @guest_3 = Guest.new('Harry')
+    @guest_4 = Guest.new('George')
+    @guest = Guest.new([@guest_1, @guest_2, @guest_3, @guest_4])
 
-    @song1_name = Song.new('Bohemian Rhapsody')
-    @song2_name = Song.new('Flash')
-    @song3_name = Song.new('Barcelona')
-    @song4_name = Song.new('Somebody to Love')
-    @song5_name = Song.new('Killer Queen')
+    @song_1 = Song.new('Bohemian Rhapsody')
+    @song_2 = Song.new('Flash')
+    @song_3 = Song.new('Barcelona')
+    @song_4 = Song.new('Somebody to Love')
+    @song_5 = Song.new('Killer Queen')
+    @song = Song.new([@song_1, @song_2, @song_3, @song_4, @song_5])
 
-    @song = Song.new([@song1_name, @song2_name, @song3_name, @song4_name, @song5_name])
-    @room_number = [1, 2]
+    @room_1 = Room.new('1')
+    @room_2 = Room.new('2')
+    @room = Room.new([@room1, @room2])
   end
+
 
 end
