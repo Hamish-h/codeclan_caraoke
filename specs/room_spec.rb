1b07fae2a04765b9cecd87895733822f2e5a53d4
Hanish-H <hamish.h@outlook.com>
2018-04-21 19:10:33 +0100

    updated room and room guests

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -8,7 +8,10 @@ require_relative('../song.rb')
 class RoomTest < MiniTest::Test
 
   def setup
-    @guest_name = Guest.new['Tom', 'Dick', 'Harry', 'George']
+    @guest1_name = Guest.new('Tom')
+    @guest2_name = Guest.new('Dick')
+    @guest3_name = Guest.new('Harry')
+    @guest4_name = Guest.new('George')
 
     @song1_name = Song.new('Bohemian Rhapsody')
     @song2_name = Song.new('Flash')
@@ -16,10 +19,8 @@ class RoomTest < MiniTest::Test
     @song4_name = Song.new('Somebody to Love')
     @song5_name = Song.new('Killer Queen')
 
-    @room_playlist = Song.new([@song1_name, @song2_name, @song3_name, @song4_name, @song5_name])
+    @song = Song.new([@song1_name, @song2_name, @song3_name, @song4_name, @song5_name])
     @room_number = [1, 2]
   end
-
-
 
 end
