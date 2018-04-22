adec53ddb600d4f32010e8917a914275cdeeadd7
Hanish-H <hamish.h@outlook.com>
2018-04-22 01:23:08 +0100

    room and room spec setup and definition updated

--- room.rb
+++ room.rb
@@ -2,11 +2,23 @@
 class Room
 
   attr_reader :room_number
-  attr_accessor :room_playlist
+  attr_accessor :song, :guest
 
-  def initialize(room_number, room_playlist)
+  def initialize(room_number, song, guest)
     @room_number = room_number
-    @room_playlist = room_playlist
+    @song = song
+    @guest = guest
   end
 
+  def check_in_guest_to_rooms(guest)
+    @guest.push(guest)
+  end
+
+  def check_out_guest_from_rooms(guest)
+    @guest.pop(guest)
+  end
+
+   def add_song_to_rooms(song)
+     @room << song
+   end
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -8,23 +8,40 @@ require_relative('../song.rb')
 class RoomTest < MiniTest::Test
 
   def setup
-    @guest_1 = Guest.new('Tom')
-    @guest_2 = Guest.new('Dick')
-    @guest_3 = Guest.new('Harry')
-    @guest_4 = Guest.new('George')
-    @guest = Guest.new([@guest_1, @guest_2, @guest_3, @guest_4])
 
     @song_1 = Song.new('Bohemian Rhapsody')
     @song_2 = Song.new('Flash')
     @song_3 = Song.new('Barcelona')
     @song_4 = Song.new('Somebody to Love')
     @song_5 = Song.new('Killer Queen')
-    @song = Song.new([@song_1, @song_2, @song_3, @song_4, @song_5])
+    @songs = Song.new([@song_1, @song_2, @song_3, @song_4, @song_5])
 
-    @room_1 = Room.new('1')
-    @room_2 = Room.new('2')
-    @room = Room.new([@room1, @room2])
+    @guest_1 = Guest.new('Tom')
+    @guest_2 = Guest.new('Dick')
+    @guest_3 = Guest.new('Harry')
+    @guest_4 = Guest.new('George')
+    @guests = Guest.new([@guest_1, @guest_2, @guest_3, @guest_4])
+
+    @room_1 = Room.new(1, @song_1, @guest_1)
+    @room_2 = Room.new(2, @song_2, @guest_2)
+
   end
 
+  def test_rooms_empty
+    assert_nil(@room_number)
+  end
+
+  def test_playlist_empty
+    assert_nil(@song)
+  end
+
+  def test_guestlist_empty
+    assert_nil(@guest)
+  end
+
+  # def test_can_create_room()
+  #   room = Room.new(1, @song_1, @guest_1)
+  #   assert_equal(Room, @room.class())
+  # end
 
 end
